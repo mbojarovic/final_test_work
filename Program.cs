c9541378674fcc4dd9b56ca137e429ea22eedba7
Marek <m.bojarovic@gmail.com>
2023-03-24 08:05:38 +0200

    добавлен метод распечатки массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿//любой массив строк
-string[] array = new string[]{"hello", "2", "world"};
+﻿void printArray(string[] array)
+{
+    Console.Write("[");
+    foreach (string elements in array)
+    {
+        Console.Write($"\"{elements}\",");
+    }
+    Console.Write("]");
+}
+
+//красивый вывод как в примере
+void printFixArray(string[] array, string[] newArray)
+{
+    printArray(array);
+    Console.Write("->");
+    printArray(newArray);
+}
+
+
